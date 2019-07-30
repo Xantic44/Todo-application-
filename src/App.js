1dc968c0fa093ea95423902bab1a87db39f0a406
Xantic44 <51379252+Xantic44@users.noreply.github.com>
2019-07-30 19:13:09 +0200

    Add files via upload
    my own Todo application. 
    next step : upload website.

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
+import './App.css';
+import './App';
+
+class App extends React.Component {
+  state= {
+    items:[],
+    id: uuid(),
+    item:'',
+    editItem:false
+  };
+
+// END OF STATE
+
+// START OF METHOD
+handleChange= (e) => {
+  this.setState({
+    item: e.target.value
+  })
+};
+
+// handleSubmit now for submit capturing !
+handleSubmit= (e) => {
+  e.preventDefault();
+
+  //create new item, new value place in new !
+  const newItem = {
+    id: this.state.id,
+    title: this.state.item
+
+  }
+
+  const updatedItems = [...this.state.items, newItem]
+
+this.setState({
+  items: updatedItems,
+  item: '',
+  id: uuid(),
+  editItem: false
+})
+};
+
+// clear erase button
+clearList = () => {
+  this.setState({
+    items: []
+  })
+};
+
+// delete tasks method by handleDelete.
+handleDelete= (id) => {
+  const filteredItems = this.state.items.filter(item => item.id !== id)
+  this.setState({
+    items: filteredItems
+  })
+};
+
+//handle edit method. for editing task if typos.
+handleEdit = id => {
+const filteredItems = this.state.items.filter(item => item.id !== id)
+
+const selectedItem = this.state.items.find(item => item.id ===   id);
+
+this.setState({
+  items: filteredItems,
+  item: selectedItem.title,
+  editItem: true,
+  id: id
+
+});
+
+
+};
+
+
+
+    render ()  {
+      return (
+    <div className="container" >
+      <div className="row">
+        <div className="col-10 mx-auto col-md-8 mt-4" >
+          <h3 className="text-capitalize text-center">todo input
+          </h3>
+    <TodoInput
+      item={this.state.item} handleChange={this.handleChange}
+      handleSubmit={this.handleSubmit}
+      editItem={this.state.editItem}
+      />
+    <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete}
+      handleEdit={this.handleEdit}/>
+        </div>
+          </div>
+          </div>
+
+
+  );
+}
+}
+
+export default App;
